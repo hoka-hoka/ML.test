c00e4da884815afe9ddf029d6473b06f9683b825
semyon <sff1@tpu.ru>
2021-05-20 19:32:11 +0700

    change the formatting of the date of birth

--- src/components/input-date/input-date.js
+++ src/components/input-date/input-date.js
@@ -1,8 +1,7 @@
-import moment from 'moment';
-import IMask from 'imask';
 import './input-date.scss';
 import DropDown from '../../common/DropDown';
 import Sibling from '../../common/Sibling';
+import DateFormat from '../../common/DateFormat';
 
 const initCalendar = (item) => {
   const $calendar = Sibling.getOlderSibling({
@@ -15,46 +14,15 @@ const initCalendar = (item) => {
     $par: $(item),
     $list: $calendar,
     $focus: $(item),
-    // aria: true,
+    aria: true,
   };
   new DropDown(listOptions);
 };
 
 const formatDate = (item) => {
   const fields = $(item).find('.input__field');
-  fields.each((_, e) => {
-    const momentFormat = e.dataset.dateFormat;
-    const mask = IMask(e, {
-      mask: momentFormat,
-      autofix: true,
-      overwrite: true,
-      blocks: {
-        ДД: {
-          mask: IMask.MaskedRange,
-          from: 1,
-          to: 31,
-          maxLength: 2,
-        },
-        ММ: {
-          mask: IMask.MaskedRange,
-          from: 1,
-          to: 12,
-          maxLength: 2,
-        },
-        ГГГГ: {
-          mask: IMask.MaskedRange,
-          from: 1900,
-          to: 9999,
-        },
-      },
-      format: function (date) {
-        moment.locale('ru');
-        return moment(date).format(momentFormat);
-      },
-      parse: function (str) {
-        return moment(str, momentFormat);
-      },
-    });
+  fields.each((_, elem) => {
+    new DateFormat(elem, elem.dataset.dateFormat);
   });
 };
 
--- src/components/reg/reg.js
+++ src/components/reg/reg.js
@@ -1,1 +1,17 @@
+import DateFormat from '../../common/DateFormat';
 import './reg.scss';
+
+const formatDate = (item) => {
+  const fields = $(item).find('.input__field');
+  fields.each((_, elem) => {
+    new DateFormat(elem, elem.dataset.dateFormat);
+  });
+};
+
+const $dateFields = $('.input-date_js');
+if ($dateFields.length) {
+  $dateFields.each((_, item) => {
+    formatDate(item);
+    // initCalendar(item);
+  });
+}
